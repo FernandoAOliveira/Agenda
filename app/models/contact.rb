0713351f9410e2b4f875863f7e8b49015b96c97b
fernandoAOliveira <fernandous4@yahoo.com.br>
2020-07-11 10:15:12 -0300

    ADD conteudo ao model Contact.rb

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,7 @@
 class Contact < ApplicationRecord
-end
+    has_many :phones # possui muitos telefones
+    has_one :address # possui um endereço
+   
+    has_many :contact_kinds # contact possui muitos <tabela intermediaris>
+    has_many :kinds, through: :contact_kinds # contact possui muitos kinds, através de <tabela intermediaris>
+  end
