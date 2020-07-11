49d300c7f0f1e6748c5a98b5983ba1505e1a264c
fernandoAOliveira <fernandous4@yahoo.com.br>
2020-07-11 10:43:52 -0300

    validations adress e phone

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,4 @@
 class Address < ApplicationRecord
     belongs_to :contact
+    validates :adress, uniqueness: true, on: :create
 end
--- app/models/phone.rb
+++ app/models/phone.rb
@@ -1,3 +1,4 @@
 class Phone < ApplicationRecord
   belongs_to :contact
+  validates :phone, uniqueness: true, on: :create
 end
