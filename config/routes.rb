57e41e07bde587aacb968b6a3e4aa75f680c7137
fernandoAOliveira <fernandous4@yahoo.com.br>
2020-07-23 20:17:56 -0300

    modificando model contact.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'contacts/index'
-  get 'contacts/show'
-  get 'contacts/new'
-  post 'contacts/create'
-  get 'contacts/edit'
-  put 'contacts/update'
-  delete 'contacts/destroy'
+  resoucers contact
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
